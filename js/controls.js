13f9eec2fb256826272c62b384fc2e0234bd877c
kenef <ken@awaken.name>
2015-09-02 15:44:44 -0800

    moved layers definitions to controls.js and added baseLayers object to pass into L.control.layers()

--- js/controls.js
+++ js/controls.js
@@ -1,3 +1,19 @@
+/* ----------------------------- Layers ------------------------*/
+
+var overlayMaps = {
+	"Restrooms": layersCollection.restrooms.layers,
+	"Portable Toilets": layersCollection.portables.layers,
+	"ATM's": layersCollection.atms.layers,
+	"Loading Zones": layersCollection.loadingZones.layers,
+	"EMS": layersCollection.ems.layers
+ }
+ 
+var grayscale = L.mapbox.tileLayer("alopexid.n9ih10h9");
+
+var baseLayers = {
+	"Greyscale": grayscale
+}
+
 /* ----------------------------- Layers Control ------------------------*/
 
-L.control.layers(null, overlayMaps).addTo(map);
+L.control.layers(baseLayers, overlayMaps).addTo(map);
--- js/layersCollections.js
+++ js/layersCollections.js
@@ -26,8 +26,8 @@ var arrLength = restroomsArr.length;
 var a = 0;
 var BATHROOMS = [];
 for (i=0; i<arrLength; i++) {
-var layer = L.marker([restroomsArr[i], restroomsArr[i+=1]], {icon: bathIcon});
-BATHROOMS.push(layer);//pushing the bathroms array item to the variable layer every iteration
+	var layer = L.marker([restroomsArr[i], restroomsArr[i+=1]], {icon: bathIcon});
+	BATHROOMS.push(layer);//pushing the bathroms array item to the variable layer every iteration
 }
 layersCollection.restrooms.layers = BATHROOMS;
 
@@ -36,8 +36,8 @@ var arrLength = portablesArr.length;
 var a = 0;
 var PORTABLES = [];
 for (i=0; i<arrLength; i++) {
-var layer = L.marker([portablesArr[i], portablesArr[i+=1]], {icon: bathIconPortable});
-PORTABLES.push(layer);//pushing the bathroms array item to the variable layer every iteration
+	var layer = L.marker([portablesArr[i], portablesArr[i+=1]], {icon: bathIconPortable});
+	PORTABLES.push(layer);//pushing the bathroms array item to the variable layer every iteration
 }
 layersCollection.portables.layers = PORTABLES;
 
@@ -46,8 +46,8 @@ var arrLength = atmArr.length;
 var a = 0;
 var ATM = [];
 for (i=0; i<arrLength; i++) {
-var layer = L.marker([atmArr[i], atmArr[i+=1]], {icon: atmIcon});
-ATM.push(layer);//pushing the bathroms array item to the variable layer every iteration
+	var layer = L.marker([atmArr[i], atmArr[i+=1]], {icon: atmIcon});
+	ATM.push(layer);//pushing the bathroms array item to the variable layer every iteration
 }
 layersCollection.atms.layers = ATM;
 
@@ -56,8 +56,8 @@ var arrLength = loadingZoneArr.length;
 var a = 0;
 var LOADING = [];
 for (i=0; i<arrLength; i++) {
-var layer = L.marker([loadingZoneArr[i], loadingZoneArr[i+=1]], {icon: loadingIcon});
-LOADING.push(layer);//pushing the bathroms array item to the variable layer every iteration
+	var layer = L.marker([loadingZoneArr[i], loadingZoneArr[i+=1]], {icon: loadingIcon});
+	LOADING.push(layer);//pushing the bathroms array item to the variable layer every iteration
 }
 layersCollection.loadingZones.layers = LOADING;
 
@@ -66,8 +66,8 @@ var arrLength = emsArr.length;
 var a = 0;
 var EMS = [];
 for (i=0; i<arrLength; i++) {
-var layer = L.marker([emsArr[i], emsArr[i+=1]], {icon: emsIcon});
-EMS.push(layer);//pushing the bathroms array item to the variable layer every iteration
+	var layer = L.marker([emsArr[i], emsArr[i+=1]], {icon: emsIcon});
+	EMS.push(layer);//pushing the bathroms array item to the variable layer every iteration
 }
 layersCollection.ems.layers = EMS;
 
