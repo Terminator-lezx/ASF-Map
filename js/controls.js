615996ab193f51ff6819bbb471d64971df8b76b3
kenef <ken@awaken.name>
2015-09-02 15:49:13 -0800

    added missing semicolons

--- js/controls.js
+++ js/controls.js
@@ -6,13 +6,13 @@ var overlayMaps = {
 	"ATM's": layersCollection.atms.layers,
 	"Loading Zones": layersCollection.loadingZones.layers,
 	"EMS": layersCollection.ems.layers
- }
+ };
  
 var grayscale = L.mapbox.tileLayer("alopexid.n9ih10h9");
 
 var baseLayers = {
 	"Greyscale": grayscale
-}
+};
 
 /* ----------------------------- Layers Control ------------------------*/
 
