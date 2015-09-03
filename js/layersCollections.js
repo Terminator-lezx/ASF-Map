cc9e575c4f2380568fbdda83c94872c17cbc52f8
Courtney Koppenhaver <ckoppenhaver@gmail.com>
2015-09-02 19:14:56 -0800

    Legend now appearing

--- js/layersCollections.js
+++ js/layersCollections.js
@@ -1,15 +1,41 @@
+var layersCollection = {
+	restrooms: {
+		name: "Restroom",
+		layers: []
+	},
+	portables: {
+		name: "Portable Restroom",
+		layers: []
+	},
+  atms: {
+	 name: "ATM",
+		layers: []
+		},
+	loadingZones: {
+	  name: "Loading Zone",
+		layers: []
+	},
+  ems: {
+	  name: "EMS",
+		layers: []
+	}
+}
+
 //restrooms
 var arrLength = restroomsArr.length;
 var a = 0;
-var bathroomsTemp = [];
+var BATHROOMS = [];
 for (i=0; i<arrLength; i++) {
-	L.marker([restroomsArr[i], restroomsArr[i+=1]], {icon: bathIcon}).addTo(map).bindPopup("Restroom");
+	var layer = L.marker([restroomsArr[i], restroomsArr[i+=1]], {icon: bathIcon});
+	BATHROOMS.push(layer);//pushing the bathroms array item to the variable layer every iteration
+	//L.marker([restroomsArr[i], restroomsArr[i+=1]], {icon: bathIcon}).addTo(map).bindPopup("Restroom");
 }
+layersCollection.restrooms.layers = BATHROOMS;
 
 //portable toilets
 var arrLength = portablesArr.length;
 var a = 0;
-var portablesTemp = [];
+var PORTABLES = [];
 for (i=0; i<arrLength; i++) {
 	L.marker([portablesArr[i], portablesArr[i+=1]], {icon: bathIconPortable}).addTo(map).bindPopup("Portable Toilet");
 }
@@ -17,7 +43,7 @@ for (i=0; i<arrLength; i++) {
 //atms
 var arrLength = atmArr.length;
 var a = 0;
-var atmTemp = [];
+var ATM = [];
 for (i=0; i<arrLength; i++) {
 	L.marker([atmArr[i], atmArr[i+=1]], {icon: atmIcon}).addTo(map).bindPopup("ATM");
 }
@@ -25,7 +51,7 @@ for (i=0; i<arrLength; i++) {
 //loading zones
 var arrLength = loadingZoneArr.length;
 var a = 0;
-var loadingTemp = [];
+var LOADING = [];
 for (i=0; i<arrLength; i++) {
 	L.marker([loadingZoneArr[i], loadingZoneArr[i+=1]], {icon: loadingIcon}).addTo(map).bindPopup("Loading Zone");
 }
@@ -33,29 +59,29 @@ for (i=0; i<arrLength; i++) {
 // ems
 var arrLength = emsArr.length;
 var a = 0;
-var emsTemp = [];
+var EMS = [];
 for (i=0; i<arrLength; i++) {
 	L.marker([emsArr[i], emsArr[i+=1]], {icon: emsIcon}).addTo(map).bindPopup("EMS");
 }
 
 //parking
-//red parking entrance 
+//red parking entrance
 L.marker([61.577785, -149.140183], {icon: redIcon}).addTo(map).bindPopup("Entrance to Red Parking Lot");
 
-// red gate 
+// red gate
 L.marker([61.577741, -149.134951], {icon: redIcon}).addTo(map).bindPopup("Red Gate Entrance into Fair Grounds");
 
-//purple parking entrance 
+//purple parking entrance
 L.marker([61.583065, -149.118112], {icon: purpleIcon}).addTo(map).bindPopup("Entrance to Purple Parking Lot");
 
-//purple gate 
+//purple gate
 L.marker([61.584362, -149.1288], {icon: purpleIcon}).addTo(map).bindPopup("Purple Gate Entrance into Fair Grounds");
 
-//yellow parking entrance 
+//yellow parking entrance
 L.marker([61.58119, -149.118144], {icon: yellowIcon}).addTo(map).bindPopup("Entrance to Yellow Parking Lot");
 
-//yellow gate 
+//yellow gate
 L.marker([61.581274, -149.122946], {icon: yellowIcon}).addTo(map).bindPopup("Yellow Gate Entrance into Fair Grounds");
 
-//green gate 
+//green gate
 L.marker([61.578405, -149.137709], {icon: greenIcon}).addTo(map).bindPopup("Green Gate Entrance into Fair Grounds");
