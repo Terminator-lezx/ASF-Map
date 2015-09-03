f89e27690bd4322812b31ede4bac1a65622e09b3
kenef <ken@awaken.name>
2015-09-02 19:19:37 -0800

    pulling commit

--- js/layersCollections.js
+++ js/layersCollections.js
@@ -43,19 +43,19 @@ for (i=0; i<arrLength; i++) {
 L.marker([61.577785, -149.140183], {icon: redIcon}).addTo(map).bindPopup("Entrance to Red Parking Lot");
 
 // red gate 
-L.marker([61.577741, -149.134951], {icon: redIcon}).addTo(map).bindPopup("Red Gate Entrance into Fair Grounds");
+L.marker([61.577741, -149.134951], {icon: redGateIcon}).addTo(map).bindPopup("Red Gate Entrance into Fair Grounds");
 
 //purple parking entrance 
 L.marker([61.583065, -149.118112], {icon: purpleIcon}).addTo(map).bindPopup("Entrance to Purple Parking Lot");
 
 //purple gate 
-L.marker([61.584362, -149.1288], {icon: purpleIcon}).addTo(map).bindPopup("Purple Gate Entrance into Fair Grounds");
+L.marker([61.584362, -149.1288], {icon: purpleGateIcon}).addTo(map).bindPopup("Purple Gate Entrance into Fair Grounds");
 
 //yellow parking entrance 
 L.marker([61.58119, -149.118144], {icon: yellowIcon}).addTo(map).bindPopup("Entrance to Yellow Parking Lot");
 
 //yellow gate 
-L.marker([61.581274, -149.122946], {icon: yellowIcon}).addTo(map).bindPopup("Yellow Gate Entrance into Fair Grounds");
+L.marker([61.581274, -149.122946], {icon: yellowGateIcon}).addTo(map).bindPopup("Yellow Gate Entrance into Fair Grounds");
 
 //green gate 
 L.marker([61.578405, -149.137709], {icon: greenIcon}).addTo(map).bindPopup("Green Gate Entrance into Fair Grounds");
